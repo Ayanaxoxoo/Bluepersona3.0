ed50b4ca8aa1b67b46d92be4253328f4ed491e9a
Ayanaxoxoo <165037059+Ayanaxoxoo@users.noreply.github.com>
2024-09-22 13:48:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,167 @@
+var agreeEI_number = 0
+var disagreeEI_number = 0
+
+function eicounter(){
+var selected_buttons_a = document.querySelectorAll('input[type="radio"]:checked[value="agree"]');
+agreeEI_number= selected_buttons_a.length 
+
+
+var selected_buttons_d =document.querySelectorAll('input[type="radio"]:checked[value="disagree"]');
+disagreeEI_number= selected_buttons_d.length 
+
+console.log(agreeEI_number)
+console.log(disagreeEI_number)
+
+localStorage.setItem("agreeEI",agreeEI_number)
+localStorage.setItem("disagreeEI",disagreeEI_number)
+
+
+}
+
+
+
+
+var agreeSN_number = 0
+var disagreeSN_number = 0
+function SNcounter(){
+var selected_buttonsSN = document.querySelectorAll('input[type="radio"]:checked[value="agreeSN"]');
+agreeSN_number = selected_buttonsSN.length 
+
+var selected_buttonsSN = document.querySelectorAll('input[type="radio"]:checked[value="disagreeSN"]');
+disagreeSN_number = selected_buttonsSN.length 
+
+
+
+localStorage.setItem("agreeSN",agreeSN_number)
+localStorage.setItem("disagreeSN",disagreeSN_number)
+
+
+
+}
+
+var agreeTF_number = 0
+var disagreeTF_number = 0
+function TFcounter(){
+var selected_buttonsTF = document.querySelectorAll('input[type="radio"]:checked[value="agreeTF"]');
+agreeTF_number = selected_buttonsTF.length 
+
+var selected_buttonsTF = document.querySelectorAll('input[type="radio"]:checked[value="disagreeTF"]');
+disagreeSN_number = selected_buttonsTF.length 
+
+localStorage.setItem("agreeTF",agreeTF_number)
+localStorage.setItem("disagreeTF",disagreeTF_number)
+
+}
+
+
+
+var agreeJP_number = 0
+var disagreeJP_number = 0
+function JPcounter(){
+var selected_buttonsJP = document.querySelectorAll('input[type="radio"]:checked[value="agreeJP"]');
+agreeJP_number = selected_buttonsJP.length 
+
+var selected_buttonsJP = document.querySelectorAll('input[type="radio"]:checked[value="disagreeJP"]');
+disagreeSN_number = selected_buttonsJP.length 
+
+localStorage.setItem("agreeJP",agreeJP_number)
+localStorage.setItem("disagreeJP",disagreeJP_number)
+}
+
+
+function finalresults(){
+var agreeEI = localStorage.getItem("agreeEI")
+var disagreeEI = localStorage.getItem("disagreeEI")
+
+var agreeSN = localStorage.getItem("agreeSN")
+var disagreeSN = localStorage.getItem("disagreeSN")
+
+var agreeTF = localStorage.getItem("agreeTF")
+var disagreeTF = localStorage.getItem("disagreeTF")
+
+var agreeJP = localStorage.getItem("agreeJP")
+var disagreeJP = localStorage.getItem("disagreeJP")
+
+console.log(agreeEI)
+console.log(disagreeEI)
+
+console.log(agreeSN)
+console.log(disagreeSN)
+
+console.log(agreeTF)
+console.log(disagreeTF)
+
+console.log(agreeJP)
+console.log(disagreeJP)
+
+
+var EI, SN, TF, JP
+
+
+if(agreeEI > disagreeEI){
+    EI = "e"
+}
+else {
+    EI="i"
+
+}
+
+if(agreeSN>disagreeSN){
+    SN = "s"
+}
+else{
+    SN = "n"
+}
+
+
+if(agreeTF>disagreeTF){
+    TF = "t"
+}
+else{
+    TF = "f"
+}
+
+if(agreeJP>disagreeJP){
+    JP = "j"
+}
+else{
+    JP = "p"
+}
+
+if((EI=="i") && (SN=="s") && (TF=="t") && (JP=="j")){
+    console.log("ISTJ")
+}
+
+
+}
+
+
+
+var imageId = ""
+function selectimage(img,x){
+imageId=img
+console.log(imageId)
+document.getElementById("outputimg").src= imageId+".png"
+localStorage.setItem("output",imageId)
+// document.getElementById("everything").style.cursor="url('https://cdn-icons-png.flaticon.com/28/7591/7591554.png'),pointer";
+var elementToChange = document.getElementsByTagName("body")[0];
+ elementToChange.style.cursor = "url('"+ x +"'), auto";
+ console.log(x)
+localStorage.setItem("cursor", x)
+}
+
+
+function petname(){
+    var name = document.getElementById("petname").value;
+    document.getElementById("petnameplace").innerHTML=name;
+    document.getElementById("petselect").style.display='none'
+}
+
+
+function petexp(){
+    var outputpic =   localStorage.getItem("output") 
+    document.getElementById("outputimg").src = outputpic+".png"
+  var c= localStorage.getItem("cursor")
+  var elementToChange = document.getElementsByTagName("body")[0];
+ elementToChange.style.cursor = "url('"+ c +"'), auto";
+}
